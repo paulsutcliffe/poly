efd13c885b9baa17c67c4a9ac35760eb18390951
Paul Sutcliffe <paul@kosmyka.com>
2013-09-17 16:50:10 -0500

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poly::Application.config.secret_token = 'd04f83f04c8379d828c35383a67d5003ed0bb42f566c7183d1ca1bfba1ac43d6cf2e04f945d4e3f3a5967b8cd6747650e8b5dc2fdf2d824269e9ca4973a79af2'
