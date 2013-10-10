5f3fd451d87220ef47ecbae768585706e2ecbc1d
Paul Sutcliffe <paul@kosmyka.com>
2013-10-10 15:41:11 -0500

    descativado el mailfrom temporalmente

--- app/models/contacto.rb
+++ app/models/contacto.rb
@@ -1,24 +1,25 @@
-class Contacto < MailForm::Base
-  #attr_accessible :apellido, :direccion, :email, :empresa, :mensaje, :nombre, :telefono
+class Contacto < ActiveRecord::Base
+  #include MailForm::Delivery
+  attr_accessible :apellido, :direccion, :email, :empresa, :mensaje, :nombre, :telefono
 
-  attribute :nombre,    :validate => true
-  attribute :apellido,  :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  #attribute :nombre,    :validate => true
+  #attribute :apellido,  :validate => true
+  #attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
 
-  attribute :mensaje
-  attribute :direccion
-  attribute :empresa
-  attribute :telefono
-  attribute :nickname,  :captcha  => true
+  #attribute :mensaje
+  #attribute :direccion
+  #attribute :empresa
+  #attribute :telefono
+  #attribute :nickname,  :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
-  def headers
-    {
-      :subject => "Mensaje desde la web",
-      :to => "paul@kosmyka.com",
-      :from => %("#{nombre} #{apellido}" <#{email}>)
-    }
-  end
+  #def headers
+  #  {
+  #    :subject => "Mensaje desde la web",
+  #    :to => "paul@kosmyka.com",
+  #    :from => %("#{nombre} #{apellido}" <#{email}>)
+  #  }
+  #end
 
 end
