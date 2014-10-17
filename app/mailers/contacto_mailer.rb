968de03c1a6e2d781b26ea006f640b05677ea27a
Paul Sutcliffe <paul@kosmyka.com>
2014-10-17 16:52:41 -0500

    email

--- app/mailers/contacto_mailer.rb
+++ app/mailers/contacto_mailer.rb
@@ -1,6 +1,6 @@
 class ContactoMailer < ActionMailer::Base
   def mensaje_recibido(contacto)
     @contacto = contacto
-    mail(:to => "info@mpsac.com.pe", :subject => "Mensaje desde la web", :replay_to => contacto.email, :from => "info@mpsac.com.pe")
+    mail(:to => "info@mpsac.com.pe", :subject => "Mensaje desde la web", :replay_to => contacto.email, :from => "mensajespoly@gmail.com")
   end
 end
